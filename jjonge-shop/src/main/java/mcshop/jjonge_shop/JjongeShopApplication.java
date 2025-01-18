952fcc44cc8f6aa9801d6ed505108c189abc6068
Mincheol Kim <106231571+mincheole@users.noreply.github.com>
2025-01-18 18:40:49 +0900

    프로젝트 생성

---
+++ jjonge-shop/src/main/java/mcshop/jjonge_shop/JjongeShopApplication.java
@@ -0,0 +1,13 @@
+package mcshop.jjonge_shop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JjongeShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JjongeShopApplication.class, args);
+	}
+
+}
---
+++ jjonge-shop/src/test/java/mcshop/jjonge_shop/JjongeShopApplicationTests.java
@@ -0,0 +1,13 @@
+package mcshop.jjonge_shop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JjongeShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
