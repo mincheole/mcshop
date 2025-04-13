361c9be91baec5f69df330565f074457bd133bef
Mincheol Kim <106231571+mincheole@users.noreply.github.com>
2025-04-13 14:46:35 +0900

    25.04.13 UI 수정
    - 회원, 상품, 주문 필드명 가운데 정렬, 구분선 추가

--- jjonge-shop/src/main/java/mcshop/jjonge_shop/controller/ItemController.java
+++ jjonge-shop/src/main/java/mcshop/jjonge_shop/controller/ItemController.java
@@ -9,10 +9,7 @@ import mcshop.jjonge_shop.service.ItemService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
